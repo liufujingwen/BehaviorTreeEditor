6b6d9bf5acea32a3c1f9a3a38d5945a9501eefb6
liufujingwen <394074705@qq.com>
2019-10-02 03:04:13 +0800

    UnityDemo

--- BehaviorTreeEditor/UnityDemo/Assets/Demo/Unit/EUnitState.cs
+++ BehaviorTreeEditor/UnityDemo/Assets/Demo/Unit/EUnitState.cs
@@ -1,8 +1,11 @@
 ﻿public enum EUnitState
 {
     None,
-    Idle,
-    Move,//移动
-    Skill,//释放技能
-    BeHit,//受击
+    Idle = 1 << 1,
+    Walk = 2 << 1,//移动
+    Run = 3 << 1,//跑
+    Skill = 4 << 1,//释放技能
+    BeHit = 5 << 1,//受击
+    PreDead = 6 << 1,//预死亡
+    Dead = 7 << 1,//死亡（躺尸）
 }
---
+++ BehaviorTreeEditor/UnityDemo/Assets/Demo/Unit/UnitStateBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class UnitStateBehaviour : StateMachineBehaviour
+{
+    [SerializeField]
+    private EUnitState UnitState;
+    public Action<Animator, EUnitState> StateChangeHanlder;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+}
